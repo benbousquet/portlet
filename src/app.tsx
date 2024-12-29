33b30218c6e21719b48fafd99628390253920679
benbousquet <bbousque@asu.edu>
2024-12-29 12:33:23 -0600

    init commit

---
+++ src/app.tsx
@@ -0,0 +1,5 @@
+import { createRoot } from "react-dom/client";
+import Home from "./screens/home";
+
+const root = createRoot(document.body);
+root.render(<Home />);
---
+++ src/screens/home.tsx
@@ -0,0 +1,9 @@
+import { useRef, useState } from "react";
+
+export default function Home() {
+  return (
+    <div className="overflow-auto flex flex-col items-center">
+      <h1 className="text-center text-3xl">Home</h1>
+    </div>
+  );
+}
